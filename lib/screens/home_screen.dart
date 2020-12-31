3cbe3db29cbd280f2b186a11792c8b7b51348925
Javier Lopez <javierl.1994@gmail.com>
2020-12-31 14:05:00 -0600

    Working on travel card widget. Adding bottom navigation bar.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,32 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:tripplanner/widgets/travel_card_widget.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 
 class HomeScreen extends StatelessWidget {
+  final iconList = <IconData>[
+    Icons.brightness_5,
+    Icons.brightness_4,
+    Icons.brightness_6,
+    Icons.brightness_7,
+  ];
+
   static const routeName = '/home';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Trip Planner'),
-          backgroundColor: Color(0xFF008081),
-        ),
-        body: Padding(
+      body: SafeArea(
+        child: Padding(
           padding: const EdgeInsets.all(10),
           child: SingleChildScrollView(
             child: Column(
               children: [
+                Text('My trips'),
                 TravelCardWidget(
                   'https://travelcravingsdotcom1.files.wordpress.com/2018/04/1024px-plaza-libertad-san-salvador.png',
-                  'San Salvador',
-                  '22/12/2020',
+                  'Viaje Biometricos',
+                  'El Salvador',
+                  '6/01/2021',
+                  '10/01/2021',
                 ),
                 TravelCardWidget(
-                    'https://upload.wikimedia.org/wikipedia/commons/5/58/Westbay2106.jpg',
-                    'Roatan',
-                    '02/02/2021'),
+                  'https://upload.wikimedia.org/wikipedia/commons/5/58/Westbay2106.jpg',
+                  'Vacaciones a Roatan!',
+                  'Honduras',
+                  '02/02/2021',
+                  '10/02/2021',
+                ),
               ],
             ),
           ),
-        ));
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      // bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+      //   itemCount: iconList.length,
+      //   tabBuilder: null,
+      //   // tabBuilder: (int index, bool isActive) {
+      //   //   final color = isActive ? HexColor('#FFA400') : Colors.white;
+      //   //   return Column(
+      //   //     mainAxisSize: MainAxisSize.min,
+      //   //     mainAxisAlignment: MainAxisAlignment.center,
+      //   //     children: [
+      //   //       Icon(
+      //   //         iconList[index],
+      //   //         size: 24,
+      //   //         color: color,
+      //   //       ),
+      //   //       const SizedBox(height: 4),
+      //   //       Padding(
+      //   //         padding: const EdgeInsets.symmetric(horizontal: 8),
+      //   //         child: AutoSizeText(
+      //   //           "brightness $index",
+      //   //           maxLines: 1,
+      //   //           style: TextStyle(color: color),
+      //   //           group: autoSizeGroup,
+      //   //         ),
+      //   //       )
+      //   //     ],
+      //   //   );
+      //   // },
+      //   backgroundColor: Colors.teal,
+      //   // activeIndex: _bottomNavIndex,
+      //   activeIndex: 1,
+      //   splashColor: Colors.teal,
+      //   // notchAndCornersAnimation: animation,
+      //   splashSpeedInMilliseconds: 300,
+      //   notchSmoothness: NotchSmoothness.defaultEdge,
+      //   gapLocation: GapLocation.center,
+      //   leftCornerRadius: 32,
+      //   rightCornerRadius: 32,
+      //   // onTap: (index) => setState(() => _bottomNavIndex = index),
+      //   onTap: null,
+      // ),
+    );
   }
 }
--- lib/widgets/travel_card_widget.dart
+++ lib/widgets/travel_card_widget.dart
@@ -3,45 +3,69 @@ import 'package:flutter/material.dart';
 class TravelCardWidget extends StatelessWidget {
   final String imageUrl;
   final String tripName;
-  final String tripDate;
+  final String countryName;
+  final String tripStartDate;
+  final String tripEndDate;
 
   TravelCardWidget(
     this.imageUrl,
     this.tripName,
-    this.tripDate,
+    this.countryName,
+    this.tripStartDate,
+    this.tripEndDate,
   );
 
   @override
   Widget build(BuildContext context) {
     print(this.imageUrl);
     return InkWell(
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-        elevation: 4,
-        child: Column(
-          children: [
-            Container(
-              height: 150,
-              width: double.infinity,
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          elevation: 4,
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15)),
-                child: FadeInImage(
-                  placeholder: AssetImage('assets/images/tripplannerlogo.png'),
-                  image: NetworkImage(imageUrl),
-                  fit: BoxFit.cover,
+                    topRight: Radius.circular(15),
+                  ),
+                  child: FadeInImage(
+                    placeholder:
+                        AssetImage('assets/images/tripplannerlogo.png'),
+                    image: NetworkImage(imageUrl),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Text(this.tripName),
-                SizedBox(width: 2),
-                Text(this.tripDate),
-              ],
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(this.tripName,
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(this.countryName),
+                        Text(this.tripStartDate + ' - ' + this.tripEndDate),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
